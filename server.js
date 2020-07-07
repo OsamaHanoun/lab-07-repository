fcdb535c1df4652fff6199f1d28a3cd1965243d2
Osama Hanoun <osamahanoun997@gmail.com>
2020-07-07 16:04:27 +0300

    remove console logging

--- server.js
+++ server.js
@@ -92,7 +92,6 @@ function trailsHandler(request, response) {
 
   getTrails(latitude, longitude)
     .then(val => {
-      console.log(val);
       response.status(200).json(val);
     });
 }
@@ -101,13 +100,9 @@ function getTrails(latitude, longitude) {
   let trailsArr = [];
   let key = process.env.TRAILS_KEY;
   let url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}04&lon=${longitude}&key=${key}`;
-console.log(url);
   return superagent.get(url)
     .then(trailsData => {
-
       let data = trailsData.body.trails;
-      console.log(trailsData.body);
-
       return data;
     })
     .then(trailsData => {
@@ -130,36 +125,6 @@ function Trail(trail) {
   this.condition_time = trail.conditionDate.split(" ")[1];
 
 }
-
-/*
-[
-  {
-    "name": "Rattlesnake Ledge",
-    "location": "Riverbend, Washington",
-    "length": "4.3",
-    "stars": "4.4",
-    "star_votes": "84",
-    "summary": "An extremely popular out-and-back hike to the viewpoint on Rattlesnake Ledge.",
-    "trail_url": "https://www.hikingproject.com/trail/7021679/rattlesnake-ledge",
-    "conditions": "Dry: The trail is clearly marked and well maintained.",
-    "condition_date": "2018-07-21",
-    "condition_time": "0:00:00 "
-  },
-  {
-    "name": "Mt. Si",
-    "location": "Tanner, Washington",
-    "length": "6.6",
-    "stars": "4.4",
-    "star_votes": "72",
-    "summary": "A steep, well-maintained trail takes you atop Mt. Si with outrageous views of Puget Sound.",
-    "trail_url": "https://www.hikingproject.com/trail/7001016/mt-si",
-    "conditions": "Dry",
-    "condition_date": "2018-07-22",
-    "condition_time": "0:17:22 "
-  },
-  ...
-] 
-*/
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
